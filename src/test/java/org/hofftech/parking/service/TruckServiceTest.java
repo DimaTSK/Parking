4508f8ac82ea9f5a13bb0617841949cd9a8d3f48
101304 <Dmitriy.Gladilin@hofftech.ru>
2024-12-18 14:44:19 +0300

    убрал  модификатор доступа

--- src/test/java/org/hofftech/parking/service/TruckServiceTest.java
+++ src/test/java/org/hofftech/parking/service/TruckServiceTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class TruckServiceTest {
+class TruckServiceTest {
 
     private TruckService truckService;
     private TruckCapacityDto truckCapacity;
