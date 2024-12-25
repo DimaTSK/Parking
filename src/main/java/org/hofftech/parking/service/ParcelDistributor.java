494fc23ddd96dab050eb9b5ce34460a2e7c90baa
101304 <Dmitriy.Gladilin@hofftech.ru>
2024-12-25 22:51:44 +0300

    правки после ревью

--- src/main/java/org/hofftech/parking/service/ParcelDistributor.java
+++ src/main/java/org/hofftech/parking/service/ParcelDistributor.java
@@ -7,6 +7,7 @@ import org.hofftech.parking.model.entity.Truck;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Slf4j
 public class ParcelDistributor {
     private final ParcelService parcelService;
@@ -17,7 +18,6 @@ public class ParcelDistributor {
 
     public void distributeParcelsEvenly(List<ParcelDto> parcelDtos, List<Truck> truckEntities) {
         if (truckEntities.isEmpty()) {
-            log.error("Количество грузовиков не может быть равно 0.");
             throw new IllegalArgumentException("Невозможно распределить посылки: нет грузовиков.");
         }
 
@@ -44,11 +44,9 @@ public class ParcelDistributor {
             Truck truck = truckEntities.get(i);
             List<ParcelDto> group = truckParcels.get(i);
 
-            log.info("Заполняем грузовик {} из пачки {} посылок.", i + 1, group.size());
             for (ParcelDto pkg : group) {
                 if (!parcelService.addParcels(truck, pkg)) {
-                    log.error("Не получилось определить посылку {} в грузовик {}.", pkg.getType(), i + 1);
-                    throw new RuntimeException("Не хватает кол-ва грузовиков для определения!");
+                    throw new RuntimeException("Не хватает количества грузовиков для определения!");
                 }
             }
         }
@@ -58,28 +56,24 @@ public class ParcelDistributor {
 
     public void placeParcels(List<ParcelDto> parcelDtoList, List<Truck> truckEntities, int maxTrucks) {
         for (ParcelDto pkg : parcelDtoList) {
-            log.info("Пытаемся разместить упаковку {} с ID {}", pkg.getType(), pkg.getId());
             boolean placed = false;
             for (Truck truck : truckEntities) {
                 if (parcelService.addParcels(truck, pkg)) {
-                    log.info("Упаковка {} с ID {} успешно размещена в существующем грузовике.", pkg.getType(), pkg.getId());
                     placed = true;
                     break;
                 }
             }
 
             if (!placed) {
-                log.info("Упаковка {} с ID {} не поместилась. Создаём новый грузовик.", pkg.getType(), pkg.getId());
                 if (truckEntities.size() < maxTrucks) {
                     Truck newTruck = new Truck();
                     if (parcelService.addParcels(newTruck, pkg)) {
                         truckEntities.add(newTruck);
-                        log.info("Упаковка {} с ID {} размещена в новом грузовике.", pkg.getType(), pkg.getId());
                     } else {
-                        log.error("Ошибка: упаковка {} с ID {} не может быть размещена даже в новом грузовике.", pkg.getType(), pkg.getId());
+                        throw new RuntimeException("Упаковка " + pkg.getType() + " с ID " + pkg.getId() +
+                                " не может быть размещена даже в новом грузовике.");
                     }
                 } else {
-                    log.error("Превышен установленный лимит грузовиков!");
                     throw new RuntimeException("Превышен лимит грузовиков: " + maxTrucks);
                 }
             }
