212ebd59e5c8cfa8b91babfc988af2bbf5ec9d2d
101304 <Dmitriy.Gladilin@hofftech.ru>
2024-12-26 00:08:13 +0300

    правки после ревью

--- src/main/java/org/hofftech/parking/util/FileWriter.java
+++ src/main/java/org/hofftech/parking/util/FileWriter.java
@@ -4,15 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
 @Slf4j
-public class ParcelFileWriter {
+public class FileWriter {
     public static void saveParcelsToFile(List<String> parcels, String outputFilePath) throws IOException {
         File outputFile = new File(outputFilePath);
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
+        try (BufferedWriter writer = new BufferedWriter(new java.io.FileWriter(outputFile))) {
             for (String line : parcels) {
                 writer.write(line);
                 writer.newLine();
