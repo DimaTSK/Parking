b66ddfb42ae325db8e501e8834f686dfa3a4c51d
101304 <Dmitriy.Gladilin@hofftech.ru>
2025-01-15 17:07:33 +0300

    правки после ревью

--- src/main/java/org/hofftech/parking/service/ParcelService.java
+++ src/main/java/org/hofftech/parking/service/ParcelService.java
@@ -10,44 +10,44 @@ import java.util.List;
 @Slf4j
 public class ParcelService {
 
-    protected boolean canAddPackage(Truck truck, Parcel pkg, int startX, int startY) {
-        log.debug("Проверяем возможность добавить упаковку {} в координаты X={}, Y={}", pkg.getName(), startX, startY);
-        List<String> shape = pkg.getUniqueShape();
+    protected boolean canAddPackage(Truck truck, Parcel parcel, int startX, int startY) {
+        log.debug("Проверяем возможность добавить упаковку {} в координаты X={}, Y={}", parcel.getName(), startX, startY);
+        List<String> shape = parcel.getUniqueShape();
         int height = shape.size();
 
-        if (checkTruckLimits(truck, pkg, startX, startY, height, shape)) return false;
+        if (checkTruckLimits(truck, parcel, startX, startY, height, shape)) return false;
 
-        if (checkForIntersection(truck, pkg, startX, startY, height, shape)) return false;
+        if (checkForIntersection(truck, parcel, startX, startY, height, shape)) return false;
 
         String topRow = shape.getFirst();
         int requiredSupport = (int) Math.ceil(topRow.length() / 2.0);
         int support = 0;
         if (startY == 0) {
-            log.debug("Упаковка {} внизу грузовика, опора не требуется.", pkg.getName());
+            log.debug("Упаковка {} внизу грузовика, опора не требуется.", parcel.getName());
             return true;
         }
 
-        return !checkForSupport(truck, pkg, startX, startY, topRow, support, requiredSupport);
+        return !checkForSupport(truck, parcel, startX, startY, topRow, support, requiredSupport);
     }
 
-    private boolean checkForSupport(Truck truck, Parcel pkg, int startX, int startY, String topRow, int support, int requiredSupport) {
+    private boolean checkForSupport(Truck truck, Parcel parcel, int startX, int startY, String topRow, int support, int requiredSupport) {
         for (int x = 0; x < topRow.length(); x++) {
             if (topRow.charAt(x) != ' ' && truck.getGrid()[startY - 1][startX + x] != ' ') {
                 support++;
             }
         }
         if (support < requiredSupport) {
-            log.debug("Упаковка {} не имеет достаточной опоры. Требуется {}, доступно {}", pkg.getName(), requiredSupport, support);
+            log.debug("Упаковка {} не имеет достаточной опоры. Требуется {}, доступно {}", parcel.getName(), requiredSupport, support);
             return true;
         }
         return false;
     }
 
-    private boolean checkForIntersection(Truck truck, Parcel pkg, int startX, int startY, int height, List<String> shape) {
+    private boolean checkForIntersection(Truck truck, Parcel parcel, int startX, int startY, int height, List<String> shape) {
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < shape.get(y).length(); x++) {
                 if (shape.get(y).charAt(x) != ' ' && truck.getGrid()[startY + y][startX + x] != ' ') {
-                    log.debug("Упаковка {} пересекается с другой посылкой", pkg.getName());
+                    log.debug("Упаковка {} пересекается с другой посылкой", parcel.getName());
                     return true;
                 }
             }
@@ -55,41 +55,41 @@ public class ParcelService {
         return false;
     }
 
-    private boolean checkTruckLimits(Truck truck, Parcel pkg, int startX, int startY, int height, List<String> shape) {
+    private boolean checkTruckLimits(Truck truck, Parcel parcel, int startX, int startY, int height, List<String> shape) {
         for (int y = 0; y < height; y++) {
             int rowWidth = shape.get(y).length();
             if (startX + rowWidth > truck.getWidth() || startY + y >= truck.getHeight()) {
-                log.debug("Упаковка {} выходит за пределы грузовика", pkg.getName());
+                log.debug("Упаковка {} выходит за пределы грузовика", parcel.getName());
                 return true;
             }
         }
         return false;
     }
 
-    protected boolean addPackage(Truck truck, Parcel pkg) {
-        log.info("Пытаемся добавить упаковку {} в грузовик.", pkg.getName());
+    protected boolean addPackage(Truck truck, Parcel parcel) {
+        log.info("Пытаемся добавить упаковку {} в грузовик.", parcel.getName());
 
-        List<String> shape = pkg.getUniqueShape();
+        List<String> shape = parcel.getUniqueShape();
         int height = shape.size();
 
         for (int startY = 0; startY <= truck.getHeight() - height; startY++) {
             for (int startX = 0; startX <= truck.getWidth() - shape.getFirst().length(); startX++) {
-                if (canAddPackage(truck, pkg, startX, startY)) {
-                    log.info("Упаковка {} успешно добавлена", pkg.getName());
-                    pkg.setParcelStartPosition(new ParcelStartPosition(startX, startY));
-                    placePackage(truck, pkg, startX, startY);
+                if (canAddPackage(truck, parcel, startX, startY)) {
+                    log.info("Упаковка {} успешно добавлена", parcel.getName());
+                    parcel.setParcelStartPosition(new ParcelStartPosition(startX, startY));
+                    placePackage(truck, parcel, startX, startY);
                     return true;
                 }
             }
         }
 
-        log.warn("Упаковка {} не смогла быть добавлена в грузовик.", pkg.getName());
+        log.warn("Упаковка {} не смогла быть добавлена в грузовик.", parcel.getName());
         return false;
     }
 
 
-    protected void placePackage(Truck truck, Parcel pkg, int startX, int startY) {
-        List<String> shape = pkg.getUniqueShape();
+    protected void placePackage(Truck truck, Parcel parcel, int startX, int startY) {
+        List<String> shape = parcel.getUniqueShape();
 
         for (int y = 0; y < shape.size(); y++) {
             for (int x = 0; x < shape.get(y).length(); x++) {
@@ -98,9 +98,9 @@ public class ParcelService {
                 }
             }
         }
-        pkg.setParcelStartPosition(new ParcelStartPosition(startX, startY));
-        truck.getParcels().add(pkg);
-        log.info("Упаковка {} размещена на грузовике", pkg.getName());
+        parcel.setParcelStartPosition(new ParcelStartPosition(startX, startY));
+        truck.getParcels().add(parcel);
+        log.info("Упаковка {} размещена на грузовике", parcel.getName());
     }
 
 
